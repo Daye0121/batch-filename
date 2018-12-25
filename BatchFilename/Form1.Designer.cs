e0f2011080f018391e69ff4b6481388b2127737f
Alan Yeh <huyij486491@gmail.com>
2018-12-25 15:30:15 +0800

    批次修改檔案名稱功能

---
+++ BatchFilename/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BatchFilename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_SelectPath = new System.Windows.Forms.TextBox();
+            this.btn_SelectPath = new System.Windows.Forms.Button();
+            this.lbl_Format = new System.Windows.Forms.Label();
+            this.txt_Format = new System.Windows.Forms.TextBox();
+            this.lbl_SelectPath = new System.Windows.Forms.Label();
+            this.btn_Rename = new System.Windows.Forms.Button();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.lbl_FormatView = new System.Windows.Forms.Label();
+            this.lbl_Remark = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_SelectPath
+            // 
+            this.txt_SelectPath.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_SelectPath.Location = new System.Drawing.Point(111, 40);
+            this.txt_SelectPath.Name = "txt_SelectPath";
+            this.txt_SelectPath.ReadOnly = true;
+            this.txt_SelectPath.Size = new System.Drawing.Size(303, 27);
+            this.txt_SelectPath.TabIndex = 0;
+            // 
+            // btn_SelectPath
+            // 
+            this.btn_SelectPath.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_SelectPath.Location = new System.Drawing.Point(420, 40);
+            this.btn_SelectPath.Name = "btn_SelectPath";
+            this.btn_SelectPath.Size = new System.Drawing.Size(87, 27);
+            this.btn_SelectPath.TabIndex = 0;
+            this.btn_SelectPath.Text = "選擇路徑";
+            this.btn_SelectPath.UseVisualStyleBackColor = true;
+            this.btn_SelectPath.Click += new System.EventHandler(this.btn_SelectPath_Click);
+            // 
+            // lbl_Format
+            // 
+            this.lbl_Format.AutoSize = true;
+            this.lbl_Format.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_Format.Location = new System.Drawing.Point(12, 84);
+            this.lbl_Format.Name = "lbl_Format";
+            this.lbl_Format.Size = new System.Drawing.Size(93, 16);
+            this.lbl_Format.TabIndex = 2;
+            this.lbl_Format.Text = "命名格式：";
+            // 
+            // txt_Format
+            // 
+            this.txt_Format.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt_Format.Location = new System.Drawing.Point(111, 81);
+            this.txt_Format.Name = "txt_Format";
+            this.txt_Format.Size = new System.Drawing.Size(303, 27);
+            this.txt_Format.TabIndex = 3;
+            // 
+            // lbl_SelectPath
+            // 
+            this.lbl_SelectPath.AutoSize = true;
+            this.lbl_SelectPath.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_SelectPath.Location = new System.Drawing.Point(12, 43);
+            this.lbl_SelectPath.Name = "lbl_SelectPath";
+            this.lbl_SelectPath.Size = new System.Drawing.Size(59, 16);
+            this.lbl_SelectPath.TabIndex = 4;
+            this.lbl_SelectPath.Text = "路徑：";
+            // 
+            // btn_Rename
+            // 
+            this.btn_Rename.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Rename.Location = new System.Drawing.Point(420, 412);
+            this.btn_Rename.Name = "btn_Rename";
+            this.btn_Rename.Size = new System.Drawing.Size(87, 27);
+            this.btn_Rename.TabIndex = 6;
+            this.btn_Rename.Text = "重新命名";
+            this.btn_Rename.UseVisualStyleBackColor = true;
+            this.btn_Rename.Click += new System.EventHandler(this.btn_Rename_Click);
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_Total.ForeColor = System.Drawing.Color.Brown;
+            this.lbl_Total.Location = new System.Drawing.Point(12, 134);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(112, 16);
+            this.lbl_Total.TabIndex = 7;
+            this.lbl_Total.Text = "總共有0個檔案";
+            // 
+            // lbl_FormatView
+            // 
+            this.lbl_FormatView.AutoSize = true;
+            this.lbl_FormatView.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_FormatView.Location = new System.Drawing.Point(138, 163);
+            this.lbl_FormatView.Name = "lbl_FormatView";
+            this.lbl_FormatView.Size = new System.Drawing.Size(24, 16);
+            this.lbl_FormatView.TabIndex = 11;
+            this.lbl_FormatView.Text = "　";
+            // 
+            // lbl_Remark
+            // 
+            this.lbl_Remark.AutoSize = true;
+            this.lbl_Remark.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_Remark.ForeColor = System.Drawing.Color.Firebrick;
+            this.lbl_Remark.Location = new System.Drawing.Point(299, 120);
+            this.lbl_Remark.Name = "lbl_Remark";
+            this.lbl_Remark.Size = new System.Drawing.Size(208, 30);
+            this.lbl_Remark.TabIndex = 12;
+            this.lbl_Remark.Text = "例如:File1, File2...，就輸入File*\r\n預設為1, 2, 3...\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 451);
+            this.Controls.Add(this.lbl_Remark);
+            this.Controls.Add(this.lbl_FormatView);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.btn_Rename);
+            this.Controls.Add(this.lbl_SelectPath);
+            this.Controls.Add(this.txt_Format);
+            this.Controls.Add(this.lbl_Format);
+            this.Controls.Add(this.btn_SelectPath);
+            this.Controls.Add(this.txt_SelectPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_SelectPath;
+        private System.Windows.Forms.Button btn_SelectPath;
+        private System.Windows.Forms.Label lbl_Format;
+        private System.Windows.Forms.TextBox txt_Format;
+        private System.Windows.Forms.Label lbl_SelectPath;
+        private System.Windows.Forms.Button btn_Rename;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.Label lbl_FormatView;
+        private System.Windows.Forms.Label lbl_Remark;
+    }
+}
+
---
+++ BatchFilename/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BatchFilename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 選擇資料夾路徑
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_SelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.txt_SelectPath.Text = path.SelectedPath;
+
+            CheckPathAndShow();
+        }
+
+        /// <summary>
+        /// 依照所需格式重新命名資料夾內所有檔案
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Rename_Click(object sender, EventArgs e)
+        {
+            if (!CheckPathAndShow()) return;
+
+            var files = Directory.GetFiles(txt_SelectPath.Text);
+            if (files.Count() == 0)
+            {
+                MessageBox.Show("資料夾內沒有檔案!");
+                return;
+            }
+
+            var seq = 1;
+            var paddingNum = files.Count().ToString().Length;
+
+            foreach (var file in files)
+            {
+                var ext = Path.GetExtension(file);
+                var serialNum = seq.ToString().PadLeft(paddingNum, '0');
+                var newFilename = txt_Format.Text;
+
+                //有*就取代換成流水號，反之加在最後面
+                if (txt_Format.Text.IndexOf("*") >= 0)
+                    newFilename = Path.Combine(txt_SelectPath.Text, $"{txt_Format.Text.Replace("*", serialNum)}{ext}");
+                else
+                    newFilename = Path.Combine(txt_SelectPath.Text, $"{txt_Format.Text}{seq}{ext}");
+
+                File.Move(file, newFilename);
+                seq = seq + 1;
+            }
+            MessageBox.Show("完成!");
+        }
+
+        /// <summary>
+        /// 檢查路徑是否合法且秀出路徑底下檔案數量
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckPathAndShow()
+        {
+            if (txt_SelectPath.Text == "" || txt_SelectPath.Text == null)
+            {
+                MessageBox.Show("路徑不得為空!");
+                return false;
+            }
+            else
+            {
+                var files = Directory.GetFiles(txt_SelectPath.Text);
+                this.lbl_Total.Text = $"總共有{files.Count()}個檔案";
+                return true;
+            }
+        }
+    }
+}
